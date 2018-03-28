e5a6e518b5c3a724e5eaaba125d01ca07938efe8
Travis Joris <tjoris2018@bklawtech.com>
2018-03-28 22:28:27 +0000

    Switched over the Java Script to Cloud 9

--- js/script.js
+++ js/script.js
@@ -5,6 +5,36 @@
 $(document).ready(function(){
   
   
-  
+  function giphyURLWithSearchTerm(searchTerm) {
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+    
+}
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('text-center').append('<img src=' + srcURL + '>');
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+           var url = response.data[0].images.original.url;
+           appendImageToBody(url);
+      },
+    }); 
+}
+
+//Add a click handler below to call the function when the button is clicked
+$("button").click(function (searchTerm) {
+    var searchTerm = $("input").val();
+    callGiphyAPIWithSearchTerm(searchTerm);
+    $('#text-center').empty();
+});
+
+    
   
 });
